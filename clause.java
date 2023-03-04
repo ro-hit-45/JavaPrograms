e07df3a3edcac2e9c61e10af8642b049a3b69185
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-03-04 16:07:39 +0530

    java program

---
+++ clause.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class clause {
+    public static void main(String[] args) {
+        boolean learning = true;
+        if (learning) {
+            System.out.println("java program");
+
+        } else {
+            System.out.println("What are you doing here");
+        }
+    }
+
+}
