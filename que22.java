c1b077a46f88108ddcfd472b98b4fe04e8a0146a
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-03-07 19:27:39 +0530

    basic of java

---
+++ que22.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class que22 {
+    public static void main(String[] args) {
+        int x, y, sum;
+        x = 25;
+        y = 75;
+        sum = x + y;
+        System.out.println("show the value of entered number= " + sum);
+
+    }
+}
