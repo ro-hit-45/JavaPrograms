a6c40cffad0f76617e01a003c5834720953384c2
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-03-13 16:19:27 +0530

    patternquestion

---
+++ ReverseParamid.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ReverseParamid {
+
+    public static void main(String[] args) {
+
+        Scanner r = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+
+        int rows = r.nextInt();
+
+        for (int i = 0; i <= rows - 1; i++) {
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(" ");
+
+            }
+
+            for (int k = 0; k <= rows - 1 - i; k++) {
+
+                System.out.print("*" + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+        r.close();
+
+    }
+
+}
