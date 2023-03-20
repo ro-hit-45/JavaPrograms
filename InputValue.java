6634be18e8dba76e87a3ebc47484cac77fb10862
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-03-20 19:05:57 +0530

    jp

---
+++ InputValue.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputValue {
+    public static void main(String[] args) {
+        int x;
+        Scanner n = new Scanner(System.in);
+        System.out.println("Enter the value");
+        x = n.nextInt();
+        System.out.println("Number" + x);
+    }
+
+}
