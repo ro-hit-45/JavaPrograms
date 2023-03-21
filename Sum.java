32275e9dc99f6bc60f738b471442a13d53a25123
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-03-21 12:07:23 +0530

    username

--- Sum.java
+++
@@ -1,7 +0,0 @@
-public class Sum {
-    public static void main(String[] args) {
-        int x = 5, sum, y = 10;
-        sum = x + y;
-        System.out.println("print the value " + sum);
-    }
-}
