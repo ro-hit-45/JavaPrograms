0af0335a68624c2588006c8d73eb73d3c5b2fb28
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-03-22 18:15:32 +0530

    evenoddprogram

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        int n = 14;
+
+        if (n == 2) {
+
+            System.out.println("Number is Even");
+        } else {
+
+            System.out.println("Number is Odd");
+        }
+    }
+}
