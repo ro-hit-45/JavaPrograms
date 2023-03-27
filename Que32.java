7a5243dc38d6a6630085353727cf296f23c616de
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-03-27 18:59:06 +0530

    inputque

---
+++ Que32.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Que32 {
+    public static void main(String[] args) {
+        String x;
+        System.out.println("Enter the name");
+        Scanner m = new Scanner(System.in);
+        x = m.nextLine();
+        System.out.println("your name is :" + x);
+    }
+
+}
