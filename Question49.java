cad1d1822eb9f87c814ba58a8977e40f6e849aef
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-05-05 00:57:47 +0530

    firstloopProgram

---
+++ Question49.java
@@ -0,0 +1,12 @@
+
+//write a java program to print 1 to 100 numbers  using loop;
+import java.util.*;
+
+class Question49 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
