b14b330f1d092ee342f05a08c30c944346f14eee
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-05-09 21:45:09 +0530

    printprimenumbers

---
+++ Question55.java
@@ -0,0 +1,12 @@
+
+//WRITE A JAVA PROGRAM TO PRINT THE ODD NUMBERS ;
+import java.util.*;
+
+class Question55 {
+    public static void main(String[] args) {
+        int a[] = { 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+        for (int c : a) {
+            System.out.println(c);
+        }
+    }
+}
