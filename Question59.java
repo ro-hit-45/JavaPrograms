dbf3acfa433cefd07d377a6f8413859b25da6914
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-05-12 00:11:40 +0530

    StringCompare

---
+++ Question59.java
@@ -0,0 +1,15 @@
+
+// WRITE A JAVA PROGRAM TO COMPARE BETWEEN STRING;
+import java.util.*;
+
+public class Question59 {
+    public static void main(String args[]) {
+        String s1 = "PYTHON";
+        String s2 = "JAVA";
+        String s3 = "REACT";
+        String s4 = "KOTLIN";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+    }
+}
---
+++ Question60.java
@@ -0,0 +1,11 @@
+// WRITE A JAVA PROGRAM TO COMPARE BETWEEN STRING;
+public class Question60 {
+    public static void main(String[] args) {
+        String s1 = "rohit";
+        String s2 = "hitman";
+        String s3 = "sharma";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s2.compareTo(s3));
+    }
+
+}
