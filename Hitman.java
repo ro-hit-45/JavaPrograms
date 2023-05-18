cb87717ca1fa375611cdbf3429153a2ed0b98165
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-05-18 22:19:05 +0530

    constructor

---
+++ Hitman.java
@@ -0,0 +1,12 @@
+//CREATEING CONSTRUCTOR METHOD IN JAVA PROGRAM. 
+public class Hitman {
+
+    // CONSTRUCTOR METHOD
+    Hitman() {
+        System.out.println("CONSTRUCTOR METHOD CALLED...");
+    }
+
+    public static void main(String[] args) {
+        Hitman object = new Hitman(); // CREATING OBJECT
+    }
+}
