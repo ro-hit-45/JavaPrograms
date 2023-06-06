f21e668e45857f433b35617df47673f9d465c072
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-06-06 22:21:53 +0530

    StringToInt

---
+++ StringToInt.java
@@ -0,0 +1,12 @@
+/*write a java program to Convert string to integer.*/
+
+import java.util.*;
+
+public class StringToInt {
+    public static void main(String args[]) {
+        String s = "500";
+        int a = Integer.parseInt(s);
+        System.out.println(s + 500);
+        System.out.println(a + 500);
+    }
+}
