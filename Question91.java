22ce1d2c01b0b1fef84f2f2f010580f808e5a041
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-06-08 07:01:46 +0530

    method

--- Question91.java
+++ Question91.java
@@ -1,13 +1,14 @@
-
-/*Method */
-import java.util.*;
-
-public class Question90 {
+public class Question91 {
     static void myMethod() {
         System.out.println("I just got executed!!!");
     }
 
     public static void main(String[] args) {
         myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
     }
+
 }
