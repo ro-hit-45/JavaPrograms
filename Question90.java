9a763e398d07b36922dfbe7bcffdcb1f7bf1a533
ro-hit-45 <114769944+ro-hit-45@users.noreply.github.com>
2023-06-08 06:55:58 +0530

    method

---
+++ Question90.java
@@ -0,0 +1,13 @@
+
+/*Method */
+import java.util.*;
+
+public class Question90 {
+    static void myMethod() {
+        System.out.println("I just got executed!!!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
